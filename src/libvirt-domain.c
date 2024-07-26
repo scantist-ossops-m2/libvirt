ce659114dfba21567f0bc1ce569bf3a689caaa4a
TL-Yao <tongle@scntist.com>
2024-07-26 16:28:44 +0000

    commit patch 9333854

--- src/libvirt-domain.c
+++ src/libvirt-domain.c
@@ -10976,6 +10976,7 @@ virDomainGetTime(virDomainPtr dom,
     virResetLastError();
 
     virCheckDomainReturn(dom, -1);
+    virCheckReadOnlyGoto(dom->conn->flags, error);
 
     if (dom->conn->driver->domainGetTime) {
         int ret = dom->conn->driver->domainGetTime(dom, seconds,
